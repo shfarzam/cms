f1271420d7e654cf4d871732da1b69484982bc9d
Shahriar Farzam <shahriar.farzam@marktplatz-mittelstand.de>
2019-04-24 12:12:15 +0200

    Initial commit for this project

---
+++ pub/index.php
@@ -0,0 +1,90 @@
+<!-- ./php/index.php -->
+
+<html>
+<head content="text/plain">
+
+    <title>Hello Docker World</title>
+</head>
+<style>
+    table, th, td {
+        border: 1px solid darkolivegreen;
+        text-align: center;
+    }
+</style>
+<body style="text-align: center;align-content: center">
+
+<?php
+echo "<p>Hello, World!</p><h1>Docker</h1>";
+
+
+$dsn = 'mysql:dbname=test_db;host=db';
+$user = 'sh';
+$password = '12345';
+
+try {
+    $link1 = new PDO($dsn, $user, $password);
+} catch (PDOException $e) {
+    echo 'Connection failed: ' . $e->getMessage();
+}
+
+echo "Connected successfully<br>";
+
+
+try {
+
+    $sql = $link1->query("CREATE TABLE IF NOT EXISTS Persons (PersonID INT,LastName VARCHAR(255),
+                          FirstName VARCHAR(255),Address VARCHAR(255),City VARCHAR(255));");
+
+    if ($sql == True) {
+
+        echo "<br>Table Persons created successfully<br>";
+    } else {
+        echo "Create down";
+    }
+
+
+} catch (PDOException $e) {
+    echo "Error creating table: " . $e->getMessage();
+}
+
+
+$sql = "INSERT INTO Persons VALUES (11, 'Far1', 'Sha1', 'allers1','Nür1'); ";
+$sql .= " INSERT INTO Persons VALUES (12, 'Far2', 'Sha2', 'allers21','Nür21');  ";
+$sql .= " INSERT INTO Persons VALUES (13, 'Far31', 'Sha31',' allers31','Nür31');";
+$sql .= " INSERT INTO Persons VALUES (14, 'Far41', 'Sha41', 'allers41','Nür41');";
+$sql .= " INSERT INTO Persons VALUES (15, 'Far51', 'Sha51', 'allers51','Nür51')";
+
+try {
+    $result = $link1->exec($sql);
+    echo "<p>Insert down successfully </p>";
+} catch (PDOException $e) {
+
+    echo "Error creating table: " . $e->getmessage();
+}
+
+
+try {
+    $sql = "SELECT PersonID,LastName,FirstName,Address,City  FROM Persons";
+    $stmt = $link1->query($sql);
+
+
+    echo "<table align='center'>";
+    echo "<tr><th>PersonID</th> <th>First Name</th>  <th>Last Name</th> </tr>";
+    // output data of each row
+
+    while ($row = $stmt->fetch()) {
+        echo "<tr>";
+        echo "<td>" . $row["PersonID"] . " </td><td> " . $row["FirstName"] . " </td><td>" . $row["LastName"] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
+
+} catch (PDOException $e) {
+
+    echo "Error" . §e . getMessage();
+}
+
+?>
+</body>
+</html>
---
+++ sql/code.sql
@@ -0,0 +1,6 @@
+CREATE TABLE Persons (PersonID int,LastName varchar(255),FirstName varchar(255),Address varchar(255),City varchar(255));
+INSERT INTO Persons VALUES (11, Far1, Sha1, allers1,Nür1);
+INSERT INTO Persons VALUES (11, Far2, Sha2, allers21,Nür21);
+INSERT INTO Persons VALUES (11, Far31, Sha31, allers31,Nür31);
+INSERT INTO Persons VALUES (11, Far41, Sha41, allers41,Nür41);
+INSERT INTO Persons VALUES (11, Far51, Sha51, allers51,Nür51);
